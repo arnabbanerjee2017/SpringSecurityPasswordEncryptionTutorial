8d36c00b831786fb3cf03a800b22bc42ff37ced8
Arnab Banerjee <arnab.ban09@gmail.com>
2019-07-19 20:57:21 +0530

    Initial commit

---
+++ src/main/java/com/arnab/spring/SpringSecurityPasswordEncryptionTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.arnab.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPasswordEncryptionTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityPasswordEncryptionTutorialApplication.class, args);
+	}
+
+}
